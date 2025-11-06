6fc12c8d7e7a4b5886f28a2cb859848d91ec8c29
tokareva-stacy <tokarevanyura@gmail.com>
2025-11-06 15:44:23 +0200

    new project

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === "production";
+
+  res.status(500).json({
+    message: isProd
+      ? "Something went wrong. Please try again later."
+      : err.message,
+  });
+});
+
+app.post('/users', (req, res) => {
+  console.log(req.body); // тепер тіло доступне як JS-об’єкт
+  res.status(201).json({ message: 'User created' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
