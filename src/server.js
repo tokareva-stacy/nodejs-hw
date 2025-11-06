8bf52f0d056199ccc1273401b363e9406a05c15e
tokareva-stacy <tokarevanyura@gmail.com>
2025-11-06 16:49:45 +0200

    Middleware +

--- src/server.js
+++ src/server.js
@@ -7,9 +7,24 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // --- Middleware ---
-app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
 
 // --- Routes ---
 app.get('/notes', (req, res) => {
