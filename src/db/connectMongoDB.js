b18c8310d4b4bf9fedc87a97ccc265e539752f79
tokareva-stacy <tokarevanyura@gmail.com>
2025-12-01 01:05:19 +0200

    connectMongoDB

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -8,7 +8,7 @@ export const connectMongoDB = async () => {
     console.log("✅ MongoDB connection established successfully");
 
     // Гарантуємо, що індекси в БД відповідають схемі
-    await Student.syncIndexes();
+    await Note.syncIndexes();
     console.log("Indexes synced successfully");
   } catch (error) {
     console.error("❌ Failed to connect to MongoDB:", error.message);
