015bbbb3d25abeada81b92c8bcd613d8708d77a7
tokareva-stacy <tokarevanyura@gmail.com>
2025-12-04 13:23:17 +0200

    request-reset-email & reset-email

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,14 +3,18 @@ import { celebrate } from 'celebrate';
 import { loginUser,
   logoutUser,
   refreshUserSession,
-  registerUser, } from '../controllers/authController.js';
-import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+  registerUser, resetPassword } from '../controllers/authController.js';
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+
 router.post('/auth/refresh', refreshUserSession);
+
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
