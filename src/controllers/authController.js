649c0258f03b4ecd9fb54f10e9dec929d4e104c2
tokareva-stacy <tokarevanyura@gmail.com>
2025-12-05 12:17:46 +0200

    userController fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,6 @@ import { User } from '../models/user.js';
 import { Session } from "../models/session.js";
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { sendEmail } from '../utils/sendMail.js';
-import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -117,11 +116,8 @@ export const requestResetEmail = async (req, res, next) => {
 
 	// 1. Формуємо шлях до шаблона
   const templatePath = path.resolve('src/templates/reset-password-email.html');
-  // 2. Читаємо шаблон
   const templateSource = await fs.readFile(templatePath, 'utf-8');
-  // 3. Готуємо шаблон до заповнення
   const template = handlebars.compile(templateSource);
-  // 4. Формуємо із шаблона HTML документ з динамічними даними
   const html = template({
     name: user.username,
     link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
@@ -132,7 +128,6 @@ export const requestResetEmail = async (req, res, next) => {
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      // 5. Передаємо HTML у функцію надписання пошти
       html,
     });
   } catch {
@@ -151,54 +146,30 @@ export const requestResetEmail = async (req, res, next) => {
 export const resetPassword = async (req, res, next) => {
 	const { token, password } = req.body;
 
-	// 1. Перевіряємо/декодуємо токен
   let payload;
   try {
     payload = jwt.verify(token, process.env.JWT_SECRET);
   } catch {
-	  // Повертаємо помилку якщо проблема при декодуванні
     next(createHttpError(401, 'Invalid or expired token'));
     return;
   }
 
-  // 2. Шукаємо користувача
   const user = await User.findOne({  _id: payload.sub,  email: payload.email });
   if (!user) {
     next(createHttpError(404, 'User not found'));
     return;
   }
 
-  // 3. Якщо користувач існує
-  // створюємо новий пароль і оновлюємо користувача
   const hashedPassword = await bcrypt.hash(password, 10);
   await User.updateOne(
 	  { _id: user._id },
 	  { password: hashedPassword }
   );
 
-  // 4. Інвалідовуємо всі можливі попередні сесії користувача
   await Session.deleteMany({ userId: user._id });
 
-	// 5. Повертаємо успішну відповідь
   res.status(200).json({
     message: 'Password reset successfully. Please log in again.',
   });
 };
 
-// аватар
-export const updateUserAvatar = async (req, res, next) => {
-  if (!req.file) {
-    next(createHttpError(400, 'No file'));
-    return;
-  }
-
-  const result = await saveFileToCloudinary(req.file.buffer);
-
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    { avatar: result.secure_url },
-    { new: true },
-  );
-
-  res.status(200).json({ url: user.avatar });
-};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
-import { updateUserAvatar } from '../controllers/authController.js';
+import { updateUserAvatar } from '../controllers/userController.js';
 import { upload } from "../middleware/multer.js";
 
 const router = Router();
